bfd5e8e22334b2c961a602a1844c3b462741f13c
administrator <liquidnuker@users.noreply.github.com>
2017-07-17 21:22:36 -0700

    added component with props

---
+++ src/vue-components/Comp4.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Comp4.vue
+    <br>
+    url: {{ prUrl }}
+    <br>
+    {{ prMessage }} 
+  </div>
+</template>
+<script>
+export default {
+    data () {
+      return {   
+        
+      }
+    },
+    props: [
+      'prMessage',
+      'prUrl'
+    ],
+    components: {
+     
+    },
+    mounted: function () {
+      
+    },
+    methods: {
+      
+    }
+  }
+</script>
--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -2,24 +2,39 @@
   <div>
 
   <component v-bind:is="currentView" />
-  <Comp2 />
-  <button v-on:click="changeView('Comp1')">change</button>
 
+  <!-- ok -->
+  <!-- <component v-bind:is="currentView" 
+  :pr-message="testMessage"
+  :pr-url="url" /> -->
+
+  <Comp2 />
+  <button v-on:click="changeView('Comp2')">change</button>
+
+  <Comp4 :pr-message="testMessage"
+  :pr-url="url" />
+  
   </div>
 </template>
 <script>
 import comp1 from "./Comp1.vue";
 import comp2 from "./Comp2.vue";
 
+// with props
+import comp4 from "./Comp4.vue";
+
   export default {
     data () {
       return {   
-        currentView: comp2
+        currentView: comp1,
+        testMessage: "testmessageZ",
+        url: "http://"
       }
     },
     components: {
       Comp1: comp1,
-      Comp2: comp2
+      Comp2: comp2,
+      Comp4: comp4
     },
     mounted: function () {
       // console.log("Home.vue mounted");
